293325a8845000d0e25bddb00cb521d17196c485
scallahan868 <socallahan@outlook.com>
2025-11-21 15:24:15 +0000

    added more test scenarios

--- airlift/single_callback.py
+++ airlift/single_callback.py
@@ -69,7 +69,7 @@ class AlgorithmTrainingCallback(RLlibCallback):
         prop_val = custom.get("proportion_deliveries_missed_mean", None)
 
         if prop_val is not None and prop_val < 0.3 and reset_iter > 20:
-            read_data["test_id"] = min(base_test_id + 1, 14)
+            read_data["test_id"] = base_test_id + 1
             reset_iter = 0
             data = {
                 "token": "aem37vgi2ahyjj13uas1rant8tm4e4",
